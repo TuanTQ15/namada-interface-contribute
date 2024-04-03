731d4f38f82da9e64f3f7c521edf8156373f59c9
Pedro Rezende <xpedrorezende@gmail.com>
2024-04-02 23:20:33 -0300

    feat(components): tweaking easings on ToggleButton

--- packages/components/src/ToggleButton.tsx
+++ packages/components/src/ToggleButton.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 import { tv } from "tailwind-variants";
 
 type Props = {
@@ -6,22 +7,24 @@ type Props = {
   onChange: () => void;
   label: string;
   theme?: "yellow" | "black";
+  containerProps?: React.ComponentPropsWithoutRef<"label">;
 };
 
 const toggleButtonClassList = tv({
   slots: {
     container: clsx(
-      "flex gap-6 items-center cursor-pointer text-sm",
+      "group flex gap-6 items-center cursor-pointer text-sm relative",
       "[&>span]:relative active:[&>span]:top-px"
     ),
     checkbox: clsx("invisible absolute pointer-events-none"),
     toggleContainer: clsx(
-      "group relative rounded-3xl bg-rblack p-1 h-5 w-10 cursor-pointer",
-      "transition-all duration-100 ease-out-quad"
+      "relative rounded-3xl bg-rblack p-1 h-5 w-10 cursor-pointer",
+      "transition-all duration-100 ease-out-quad",
+      "[&~span]:top-px"
     ),
     toggleIndicator: clsx(
       "absolute left-0.5 top-0.5 h-[calc(100%-4px)] bg-yellow",
-      "aspect-square rounded-full transition-all duration-300 ease-out-quad"
+      "aspect-square rounded-full transition-all duration-200 ease-out-quad"
     ),
   },
   variants: {
@@ -37,12 +40,19 @@ export const ToggleButton = ({
   checked,
   onChange,
   label,
+  containerProps = {},
 }: Props): JSX.Element => {
   const { container, checkbox, toggleContainer, toggleIndicator } =
     toggleButtonClassList({ checked });
 
+  const { className: containerClassName, ...containerPropsRest } =
+    containerProps;
+
   return (
-    <label className={container()}>
+    <label
+      className={twMerge(container(), containerClassName)}
+      {...containerPropsRest}
+    >
       <span>{label}</span>
       <input
         aria-label={label}
